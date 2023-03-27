4d1901451d82143282d1bdcf648d16d4dcc4bc15
UrbanBobek <bobek.urban@gmail.com>
2023-03-27 22:22:02 +0200

    Add some spicy remaps

--- lua/ubobek/remap.lua
+++ lua/ubobek/remap.lua
@@ -12,3 +12,18 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Does not overwrite buffer when pasting over something
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+-- Yank into system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+y")
+
+-- Same as above but for deleting
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Something something tmux
+--vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessioneizer<CR>")
+
+-- Creates a template for finding and replacing the word the command was issued on
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
