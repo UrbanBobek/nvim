c6e081f4c38dc86a5c9cb4fa8bd1dd1f859e7763
UrbanBobek <bobek.urban@gmail.com>
2023-04-20 10:35:56 +0200

    Add some custom spice to colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,17 @@
 
-function ColorMyPencils(color)
-	color = color or "gruvbox"
-	vim.cmd.colorscheme(color)
+require("gruvbox").setup({
+    palette_overrides = {
+        bright_green = "#88b10a",
+    }
+})
+vim.cmd("colorscheme gruvbox")
 
---	vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
---	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
-end
-
-ColorMyPencils()
+--function ColorMyPencils(color)
+--	color = color or "gruvbox"
+--	vim.cmd.colorscheme(color)
+--
+----	vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
+----	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
+--end
+--
+--ColorMyPencils()
